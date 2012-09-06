5e62de53af12d9b286b4168da3b017308ec0d6a4
hamadu <dercll1020@gmail.com>
2012-09-06 08:45:53 +0800

    implemented toolbox

--- js/painter.js
+++ js/painter.js
@@ -2,8 +2,10 @@ var CANVAS_SIZE = 512;
 var PREVIEW_SIZE = 128;
 var _scaling = 32;
 var _is_drawing = false;
+var _draw_from = [];
 var _offset_x = 0;
 var _offset_y = 0;
+var _tool = "";
 
 window.onload = function() {
   setupScale(8);
@@ -15,6 +17,8 @@ window.onload = function() {
   setupPicker();
   
   setupFileLoader();
+  
+  setupToolbox();
 }
 
 var setupPalette = function() {
@@ -60,6 +64,10 @@ var rgb = function(r, g, b) {
   return "rgb(_r, _g, _b)".replace("_r", r).replace("_g", g).replace("_b", b);
 }
 
+var rgba = function(r, g, b, a) {
+  return "rgba(_r, _g, _b, _a)".replace("_r", r).replace("_g", g).replace("_b", b).replace("_a", a);
+}
+
 var setupGrid = function(scaling) {
   var dotSize = scaling;
   
@@ -94,24 +102,21 @@ var getClickPosition = function(e) {
 
 var setupPaintEvent = function() {
   $('#gridcanvas').mousedown(function(e) {
-    var position = getClickPosition(e);
-    dotCurrentColor(position[0], position[1]);
-    _is_drawing = true;
+    drawStart(getClickPosition(e));
   });
 
   $('#gridcanvas').mousemove(function(e) {
     if (_is_drawing) {
-      var position = getClickPosition(e);
-      dotCurrentColor(position[0], position[1]);
+      drawing(getClickPosition(e));
     }
   });
 
   $('#gridcanvas').mouseup(function(e) {
-    _is_drawing = false;
+    drawEnd();
   });
 
   $('#gridcanvas').mouseout(function(e) {
-    _is_drawing = false;
+    drawEnd();
   });
 }
 
@@ -244,4 +249,247 @@ var setupCliprect = function(scaling) {
       clip(_offset_x, _offset_y, _scaling);
     }
   });
+}
+
+var setupToolbox = function() {
+  $("#toolbox_mainmenu a.btn").click(function(){
+    $("#toolbox_mainmenu a.btn").removeClass("active btn-info");
+    $(this).addClass("active btn-info");
+  });
+}
+
+var backupCurrent = function() {
+  var currentCanvas = $("#previewcanvas")[0];
+  var backupCanvas = $("<canvas/>").addClass("backupcanvas").attr("width", PREVIEW_SIZE).attr("height", PREVIEW_SIZE)[0];
+  var backupCtx = backupCanvas.getContext('2d');
+  backupCtx.drawImage(currentCanvas, 0, 0);
+  
+  $("#backupcanvas_container").append(backupCanvas);
+}
+
+var restoreBackup = function() {
+  var lastBackupCanvas = $("canvas.backupcanvas :last")[0];
+  var previewCanvas = $('#previewcanvas')[0];
+  var previewCtx = previewCanvas.getContext('2d');
+  previewCtx.clearRect(0, 0, PREVIEW_SIZE, PREVIEW_SIZE);
+  previewCtx.drawImage(lastBackupCanvas, 0, 0);
+}
+
+var removeLastBackup = function() {
+  $("canvas.backupcanvas :last").remove();
+}
+
+var getCurrentTool = function() {
+  return $("#toolbox_mainmenu .active").attr("name");
+}
+
+var drawStart = function(position) {
+  var tool = getCurrentTool();
+  if (tool == "spoit") {
+    drawSpoit(position[0], position[1]);
+    return;
+  }
+  
+  backupCurrent();
+
+  if (tool == "pencil") {
+    dotCurrentColor(position[0], position[1]);
+  } else if (tool == "bucket") {
+    drawBucket(position[0], position[1]);
+  }
+
+  _is_drawing = true;
+  _draw_from = position;
+}
+
+var drawing = function(position) {
+  var tool = getCurrentTool();
+  if (tool == "pencil") {
+    dotCurrentColor(position[0], position[1]);
+  } else if (tool == "line") {
+    restoreBackup();
+    drawLine(_offset_x + _draw_from[0], _offset_y + _draw_from[1], _offset_x + position[0], _offset_y + position[1]);
+  } else if (tool == "rect") {
+    restoreBackup();
+    drawRect(_offset_x + _draw_from[0], _offset_y + _draw_from[1], _offset_x + position[0], _offset_y + position[1]);    
+  } else if (tool == "oval") {
+    restoreBackup();
+    drawOval(_offset_x + _draw_from[0], _offset_y + _draw_from[1], _offset_x + position[0], _offset_y + position[1]);    
+  }
+}
+
+var drawEnd = function() {
+  _is_drawing = false;
+  var tool = getCurrentTool();
+  if (tool == "pencil") {
+  } else if (tool == "line") { 
+  } else if (tool == "rect") { 
+  }
+  return _is_drawing;
+}
+
+var drawLine = function(fx, fy, tx, ty) {
+  var previewCanvas = $('#previewcanvas')[0];
+  var previewCtx = previewCanvas.getContext('2d');
+  var currentColor = $(".colorbox.selected").css("background-color");
+  previewCtx.fillStyle = currentColor;
+
+  if (tx == fx) {
+    for (var y = fy ; ; y += (ty > fy) ? 1 : -1) {
+      previewCtx.fillRect(fx, y, 1, 1);
+      if (y == ty) {
+        break;
+      }
+    }    
+  } else {
+    var dy = 1.0 * (ty - fy) / (Math.abs(tx - fx) + 1);
+    var nowy = fy;
+    for (var x = fx ; ; x += (tx > fx) ? 1 : -1) {
+      var tty = fy+dy*(Math.abs(x-fx)+1)-((ty > fy) ? 0.5 : -0.5);
+      previewCtx.fillRect(x, nowy, 1, 1);
+      while (true) {
+        if (dy > 0) {
+          if (nowy >= tty) {
+            break;
+          }
+          previewCtx.fillRect(x, nowy, 1, 1);
+          nowy++;
+        } else {
+          if (nowy <= tty) {
+            break;
+          }
+          previewCtx.fillRect(x, nowy, 1, 1);
+          nowy--;
+        }
+      }
+      if (x == tx) {
+        break;
+      }
+    }
+  }
+  clip(_offset_x, _offset_y, _scaling);
+}
+
+var drawRect = function(fx, fy, tx, ty) {
+  var previewCanvas = $('#previewcanvas')[0];
+  var previewCtx = previewCanvas.getContext('2d');
+  var currentColor = $(".colorbox.selected").css("background-color");
+  previewCtx.fillStyle = currentColor;
+  previewCtx.fillRect(Math.min(fx,tx), Math.min(fy,ty), Math.abs(tx-fx)+1, Math.abs(ty-fy)+1);
+  clip(_offset_x, _offset_y, _scaling);
+}
+
+var drawOval = function(fx, fy, tx, ty) {
+  var previewCanvas = $("#previewcanvas")[0];
+  var previewCtx = previewCanvas.getContext('2d');
+  var currentColor = $(".colorbox.selected").css("background-color");
+  previewCtx.fillStyle = currentColor;
+
+  if (fx > tx) {
+    var tmp = tx;
+    tx = fx;
+    fx = tmp;
+  }
+  if (fy > ty) {
+    var tmp = ty;
+    ty = fy;
+    fy = tmp;
+  }
+  if (tx == fx) {
+    for (var y = fy ; y <= ty ; y++) {
+      previewCtx.fillRect(fx, y, 1, 1);
+    }    
+  } else {
+    var a = (tx - fx) / 2;
+    var b = (ty - fy) / 2;
+    var cx = fx + a;
+    var cy = fy + b;
+    var prevy1 = Math.floor(cy+0.5);
+    var prevy2 = Math.floor(cy-0.5);
+    for (var x = fx ; x <= cx ; x++) {
+      var dy = b*Math.sqrt(1.0-((0.5+x-cx)*(0.5+x-cx)/(a*a)));
+      previewCtx.fillRect(x, prevy1, 1, 1);
+      previewCtx.fillRect(tx-(x-fx), prevy1, 1, 1);
+      previewCtx.fillRect(x, ty-(prevy1-fy), 1, 1);
+      previewCtx.fillRect(tx-(x-fx), ty-(prevy1-fy), 1, 1);
+      while (prevy1 < ty) {
+        if (prevy1 > Math.floor(cy+dy)) {
+          break;
+        }
+        previewCtx.fillRect(x, prevy1, 1, 1);
+        previewCtx.fillRect(tx-(x-fx), prevy1, 1, 1);
+        previewCtx.fillRect(x, ty-(prevy1-fy), 1, 1);
+        previewCtx.fillRect(tx-(x-fx), ty-(prevy1-fy), 1, 1);
+        prevy1++;
+      }
+    }
+  }
+  clip(_offset_x, _offset_y, _scaling);
+}
+
+var getRGBStringFromDataArray = function(array, x, y, w, h) {
+  var rgbarr = getRGBAFromDataArray(array, x, y, w, h);
+  return rgb(rgbarr[0],rgbarr[1],rgbarr[2]);
+}
+
+var getRGBAStringFromDataArray = function(array, x, y, w, h) {
+  var rgbarr = getRGBAFromDataArray(array, x, y, w, h);
+  return rgba(rgbarr[0],rgbarr[1],rgbarr[2], rgbarr[3]);
+}
+
+var getRGBAFromDataArray = function(array, x, y, w, h) {
+  var r = array[(y*w*4)+x*4];
+  var g = array[(y*w*4)+x*4+1];
+  var b = array[(y*w*4)+x*4+2];
+  var a = array[(y*w*4)+x*4+3];
+  return [r,g,b,a];
+}
+
+var drawBucket = function(cx, cy) {
+  var previewCanvas = $("#previewcanvas")[0];
+  var previewCtx = previewCanvas.getContext('2d');
+  var currentColor = $(".colorbox.selected").css("background-color");
+  previewCtx.fillStyle = currentColor;
+  
+  var dataArray = previewCtx.getImageData(0, 0, PREVIEW_SIZE, PREVIEW_SIZE).data;
+  var dx = [1, 0, -1, 0];
+  var dy = [0, 1, 0, -1];
+  var baseColor = getRGBAStringFromDataArray(dataArray, cx, cy, PREVIEW_SIZE, PREVIEW_SIZE);
+  var flagArray = Array(PREVIEW_SIZE * PREVIEW_SIZE);
+  var queueArray = Array(PREVIEW_SIZE * PREVIEW_SIZE * 2);
+  var queueFront = 0;
+  var queueEnd = 0;
+  queueArray[queueEnd++] = cx;
+  queueArray[queueEnd++] = cy;
+  previewCtx.fillRect(cx, cy, 1, 1);
+  flagArray[cy*PREVIEW_SIZE+cx] = true;
+  while (queueFront < queueEnd) {
+    var x = queueArray[queueFront++];
+    var y = queueArray[queueFront++];
+    for (var d = 0 ; d < 4 ; d++) {
+      var tx = x + dx[d];
+      var ty = y + dy[d];
+      if (tx < 0 || ty < 0 || tx >= PREVIEW_SIZE || ty >= PREVIEW_SIZE) {
+        continue;
+      }
+      if (!flagArray[ty*PREVIEW_SIZE+tx] && getRGBAStringFromDataArray(dataArray, tx, ty, PREVIEW_SIZE, PREVIEW_SIZE) == baseColor) {
+        flagArray[ty*PREVIEW_SIZE+tx] = true;
+        queueArray[queueEnd++] = tx;
+        queueArray[queueEnd++] = ty;
+        previewCtx.fillRect(tx, ty, 1, 1);
+      }
+    }
+  }
+  clip(_offset_x, _offset_y, _scaling);
+}
+
+var drawSpoit = function(cx, cy) {
+  var previewCanvas = $("#previewcanvas")[0];
+  var previewCtx = previewCanvas.getContext('2d');
+  var currentColor = $(".colorbox.selected").css("background-color");
+  var dataArray = previewCtx.getImageData(0, 0, PREVIEW_SIZE, PREVIEW_SIZE).data;
+  var rgbarr = getRGBAFromDataArray(dataArray, cx, cy, PREVIEW_SIZE, PREVIEW_SIZE);
+  
+  addPalette(rgbarr[0], rgbarr[1], rgbarr[2]).click();
+  
 }
